505a912a76f560ba9c495fef5a1193596c35fcfd
Bryan <bolivarbryan@gmail.com>
2015-11-25 21:35:25 -0500

    first commit

---
+++ Leox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Leox
+//
+//  Created by Bryan A Bolivar M on 11/25/15.
+//  Copyright © 2015 OfferAppSAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Leox/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Leox
+//
+//  Created by Bryan A Bolivar M on 11/25/15.
+//  Copyright © 2015 OfferAppSAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

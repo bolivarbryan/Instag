788e6381d41d4c5978e737bd4941a0d9a797ffe5
Bryan <bolivarbryan@gmail.com>
2015-11-25 21:48:45 -0500

    added colors category

---
+++ Leox/Catoegories/UIColor+LXTheme.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+LXTheme.h
+//  Leox
+//
+//  Created by Bryan A Bolivar M on 11/25/15.
+//  Copyright © 2015 OfferAppSAS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (LXTheme)
+
+@end
--- Leox/Catoegories/UIColor+LXTheme.m
+++ Leox/Catoegories/UIColor+LXTheme.m
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  UIColor+LXTheme.m
 //  Leox
 //
 //  Created by Bryan A Bolivar M on 11/25/15.
 //  Copyright © 2015 OfferAppSAS. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "UIColor+LXTheme.h"
 
-@interface ViewController : UIViewController
-
+@implementation UIColor (LXTheme)
 
 @end
-
